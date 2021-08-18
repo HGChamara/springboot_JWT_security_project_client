64fc79fc0b8ec21e91e6cbe10fd75cde81e6154d
Givantha C <givantha.chamara@hitachi-dps.com>
2021-08-18 06:24:48 +0530

    base project

---
+++ src/main/java/com/putchar/securityclient/SecurityclientApplication.java
@@ -0,0 +1,13 @@
+package com.putchar.securityclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityclientApplication.class, args);
+	}
+
+}
